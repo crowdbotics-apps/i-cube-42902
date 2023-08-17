d946bff502a64cbc7cac45d2825da85352b6b536
Crowdbotics <team@crowdbotics.com>
2023-08-17 15:27:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "i-cube-42902",
+  url: "https://i-cube-42902.botics.co",
+  api: "https://i-cube-42902.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
